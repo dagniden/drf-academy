cfd6c0207e71f89ce6fc7cd77c7856135b78f21c
Daniil Gnidenko <dagnidenko@gmail.com>
2026-02-14 16:57:09 +0300

    Изменен сериализатор курса.

--- academy/serializers.py
+++ academy/serializers.py
@@ -4,10 +4,14 @@ from academy.models import Lesson, Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    number_of_lessons = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
 
+    def get_number_of_lessons(self, instance):
+        return instance.lessons.count()
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
